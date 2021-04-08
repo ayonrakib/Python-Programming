e12c95e5d390b4e020f5169e0a5edac2bb77d109
MSI\rakib <ayonrakib@gmail.com>
2021-04-08 13:05:08 -0400

    no message

---
+++ Practice/jquery.js
@@ -0,0 +1,39 @@
+
+// $(document).ready(function(){
+//     console.log($("a").attr("target"))
+//     $("h1").css("color","blue")
+
+    // $("button").click(function(){
+    //     $(".first").toggle(1000)
+    // });
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         newText = $("#dib1").html("<p> hey there </p>");
+//         console.log(newText)
+
+//     })
+
+// });
+
+// function addition(number1, number2){
+//     return number1 + number2
+// }
+// console.log(addition(20, 30))
+
+// //  function = input name jodi number hoy tahole error dekhabe 
+// function myfunction(){
+//     var name = document.getElementById("nm").value ;
+//     try {
+//         if(isNaN(name) == false) throw "its a number";
+//         else{
+//             document.getElementById("p1").innerHTML = name;
+//         }
+
+//     }
+    
+//     catch(err) {
+//         document.getElementById("p1").innerHTML = err;
+//     }
+// }
+var object1 = document.getElementById("dib1");
+console.log(object1);
