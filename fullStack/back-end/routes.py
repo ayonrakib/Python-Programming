5af9868214a62289adf007bf1a720d16d237f39a
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-02-12 23:48:35 -0600

    s3 file handling

--- fullStack/back-end/routes.py
+++ fullStack/back-end/routes.py
@@ -1,4 +1,6 @@
 
+
+from fileinput import filename
 from flask import Flask, request
 from flask_cors import CORS
 import boto3, os
@@ -8,11 +10,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 UPLOAD_FOLDER = 'D:/Coding/PythonProgramming/fullStack/back-end/static/files'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', '.js'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'js'}
 
 
 os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
-
 
 
 app = Flask("testApplication")
@@ -39,7 +40,7 @@ def allowed_file(filename):
 
 @app.route("/")
 def home():
-    return "My name is Rakib!(came from server)"
+    return "My name is Rakib! (came from server)"
 
 
 @app.route("/get-square-of-number", methods = ["POST"])
@@ -56,7 +57,7 @@ def uploadInS3():
     if request.method == 'POST':
         # check if the post request has the file part
         print("arrived in upload-file-in-s3 url")
-        print(request.files['selectedFile'])
+        # print(request.files['selectedFile'])
         if 'selectedFile' not in request.files:
             return "no files!"
         file = request.files['selectedFile']
@@ -80,6 +81,16 @@ def uploadInS3():
             return "file was not saved!"
 
     
+@app.route("/get-buckets")
+def getBuckets():
+    bucketsObject = s3.list_buckets()
+    buckets = {}
+    bucketNumber = 1
+    for bucketObject in bucketsObject["Buckets"]:
+        buckets[f"bucket{bucketNumber}"] = bucketObject["Name"]
+        bucketNumber += 1
+    return buckets
+
 
 @app.route("/create-bucket")
 def createBucket():
@@ -89,16 +100,46 @@ def createBucket():
         print("region: ",my_region)
         # region = "us-east-1"
         # location = {'LocationConstraint': region}
+        buckets = getBuckets()
+        for bucketName in buckets.values():
+            if bucketName == "flask-bucket-of-asdtoyihjerdoiuj":
+                return "bucket already exists!"
         s3.create_bucket(Bucket="flask-bucket-of-asdtoyihjerdoiuj")
         # Bucket names are GLOBALLY unique! AWS CLI will give you the IllegalLocationConstraintException 
         # if you collide with an already existing bucket and you've specified a region different than the region of the already existing bucket. 
         # If you happen to guess the correct region of the existing bucket it will give you the BucketAlreadyExists exception.
-        buckets = s3.list_buckets()
-        print("buckets: ",buckets)
     except ClientError as e:
         print(e)
-        return "failed to create bucket!"
+        return "Could not connect to S3"
     return "created bucket!"
+    
+
+
+@app.route("/get-files")
+def getFiles():
+    fileObjects = s3.list_objects(Bucket="ayonbucket")['Contents']
+    fileNames = {}
+    fileNumber = 1
+    for fileObject in fileObjects:
+        key = f"file number{fileNumber}"
+        fileNames[key] = fileObject["Key"]
+        fileNumber += 1
+    return fileNames
+
+
+@app.route("/download-file", methods = ["POST"])
+def downloadFile():
+    if request.method == "POST":
+        data = request.get_json()
+        files = getFiles()
+        fileName = data["name"]
+        for fileNameInS3 in files.values():
+            if fileNameInS3 == fileName:
+                s3 = boto3.resource('s3', aws_access_key_id=os.getenv('S3_KEY') , aws_secret_access_key=os.getenv('S3_SECRET'))
+                s3.Bucket("ayonbucket").download_file(f"{fileName}", f"D:/Coding/PythonProgramming/fullStack/back-end/static/files/downloaded-from-s3/{fileName}")
+                return "file was downloaded!"
+        return "file was not found!"
+        
 
 
 if __name__ == "__main__":
